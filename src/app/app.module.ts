3476848dc698bf765898b73eb9f34413ae7393b2
ElishaBartov <elisha.bartov@emerson.com>
2022-09-05 23:18:29 +0300

    Created 5 components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { TabsComponent } from './tabs/tabs.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactmeComponent } from './contactme/contactme.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    TabsComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactmeComponent
   ],
   imports: [
     BrowserModule
